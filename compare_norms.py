a59d180d0d1e80b00c47b568e294ed1610504bd6
Kivisaari Sasa <sasa.kivisaari@aalto.fi>
2019-03-13 10:56:07 +0200

    Some update

--- compare_norms.py
+++ compare_norms.py
@@ -13,49 +13,58 @@ import numpy as np
 from scipy.stats import spearmanr
 import scipy.cluster.hierarchy as hac
 from scipy.cluster.hierarchy import dendrogram, leaves_list
+import argparse
+import os
 
-dist = "cosine"
-norms1 = "w2v_eng"
-norms2 = "vinson"
 normpath = '/m/nbe/project/aaltonorms/data/'
-
+#Distance metric
+dist = "cosine"
 #Directory for the figures to be created
 figure_dir = '/m/nbe/project/aaltonorms/figures/'
 
+parser = argparse.ArgumentParser(description='Learn a mapping from one norm dataset to another')
+parser.add_argument('norm1', type=str, help='Norm data set 1')
+parser.add_argument('norm2', type=str, help='Norm data set 2')
+parser.add_argument('--reg', action='store_true', help='Whether to use regularization')
+args = parser.parse_args()
 
-#Get data from files
+print('Comparing', os.path.basename(args.norm1), 'to', os.path.basename(args.norm2))
+norm1 = args.norm1
+norm2 = args.norm2
+
+#Get data from the big excel file
 LUT = pd.read_excel('/m/nbe/project/aaltonorms/data/SuperNormList.xls', encoding='utf-8', 
                        header=0, index_col=0)
-norms1_vocab = pd.read_csv(normpath + norms1 + '/' + 'vocab.csv', encoding='utf-8', 
+
+#Exclude homonyms
+LUT = LUT[LUT['homonym_all']==0]
+
+norm1_vocab = pd.read_csv(normpath + norm1 + '/' + 'vocab.csv', encoding='utf-8', 
                          delimiter = '\t', header=None, index_col=0)
 
-norms2_vocab = pd.read_csv(normpath + norms2 + '/' + 'vocab.csv', encoding='utf-8', 
+norm2_vocab = pd.read_csv(normpath + norm2 + '/' + 'vocab.csv', encoding='utf-8', 
                          delimiter = '\t', header=None, index_col=0)
 
-norms1_vecs = pd.read_csv(normpath + norms1 + '/' + 'vectors.csv', encoding='utf-8', delimiter = '\t', 
+norm1_vecs = pd.read_csv(normpath + norm1 + '/' + 'vectors.csv', encoding='utf-8', delimiter = '\t', 
                          header=None, index_col=None)
 
-norms2_vecs = pd.read_csv(normpath + norms2 + '/' + 'vectors.csv', encoding='utf-8', 
+norm2_vecs = pd.read_csv(normpath + norm2 + '/' + 'vectors.csv', encoding='utf-8', 
                          delimiter = '\t', header=None, index_col=None)
 
-
-print("Norms compared: " + norms1 + " " + norms2)
-
-picks = LUT[LUT[norms1].notnull() & LUT[norms2].notnull()]
-
+picks = LUT[LUT[norm1].notnull() & LUT[norm2].notnull()]
 picks = picks.sort_values(by=["category"])
 
 #Set word label as index
-norms1_vecs.set_index(norms1_vocab.index, inplace=True)
-norms2_vecs.set_index(norms2_vocab.index, inplace=True)
+norm1_vecs.set_index(norm1_vocab.index, inplace=True)
+norm2_vecs.set_index(norm2_vocab.index, inplace=True)
 
-norms1_vecs = norms1_vecs.loc[picks[norms1]]
-norms2_vecs = norms2_vecs.loc[picks[norms2]]
+norm1_vecs = norm1_vecs.loc[picks[norm1]]
+norm2_vecs = norm2_vecs.loc[picks[norm2]]
 
 
 #Check dimensions
-assert len(norms1_vecs) == len(norms2_vecs)
-print("Number of overlapping words: " + str(len(norms1_vecs)))
+assert len(norm1_vecs) == len(norm2_vecs)
+print("Number of overlapping words: " + str(len(norm1_vecs)))
 
 
 def get_distances(norms):
@@ -97,7 +106,7 @@ def compare_norms(A,B, label_A, label_B,  cats=None):
     rho, pval = spearmanr(get_distances(A)[1], get_distances(B)[1])
    
     print(label_A + " vs. " + label_B + "  rho is: " + 
-        str(round(rho,2)) + ", pvalue = " + str(round(pval,2)))
+        str(round(rho,2)) + ", pvalue = " + str(round(pval,5)))
     plt.savefig(figure_dir + label_A + "_" + label_B + "_production_norm_comparison.pdf", 
                 format='pdf', dpi=1000, bbox_inches='tight')
 
@@ -124,18 +133,14 @@ def hierarchical_clustering(norms,labels,font):
     return index,Z
 
 
-norms1_index,norms1_Z = hierarchical_clustering(norms1_vecs.values, picks['eng_name'].tolist(),'8.')
-plt.savefig(figure_dir + norms1 + "_hierarchical_clustering.pdf", 
+norm1_index,norm1_Z = hierarchical_clustering(norm1_vecs.values, picks['eng_name'].tolist(),'8.')
+plt.savefig(figure_dir + norm1 + "_hierarchical_clustering.pdf", 
             format='pdf', dpi=1000, bbox_inches='tight')
-norms2_index,norms2_Z = hierarchical_clustering(norms2_vecs.values, picks['eng_name'].tolist(),'8.')
-plt.savefig(figure_dir + norms2 + "_hierarchical_clustering.pdf", 
+norm2_index,norm2_Z = hierarchical_clustering(norm2_vecs.values, picks['eng_name'].tolist(),'8.')
+plt.savefig(figure_dir + norm2 + "_hierarchical_clustering.pdf", 
             format='pdf', dpi=1000, bbox_inches='tight')
 
-compare_norms(norms1_vecs.values, norms2_vecs.values, norms1,norms2)
-
-
-
-
+compare_norms(norm1_vecs.values, norm2_vecs.values, norm1,norm2)
 
 def get_different_clusters(Z):
     D1=dendrogram(Z,
@@ -153,85 +158,3 @@ def get_different_clusters(Z):
     from itertools import groupby
     n = [list(group) for key, group in groupby(D2['ivl'],lambda x: x in D1['ivl'])]
     return n
-#
-#n = get_different_clusters(Z_cslb)
-#m = get_different_clusters(Z_vinson)
-#
-#clusters_cslb = []
-#for k in n:
-#    group = []
-#    for i in k:
-#        group.append(int(i))
-#    clusters_cslb.append(group)
-##print(clusters_cslb)
-#
-#clusters_vinson = []
-#for k in m:
-#    group = []
-#    for i in k:
-#        group.append(int(i))
-#    clusters_vinson.append(group)
-##print(clusters_vinson)
-#
-#cslb_hierarchial_word = []
-#cslb_hierarchial_group = []
-#for k in clusters_cslb:
-#    group =[]
-#    for i in k:
-#        group.append(cslb_list[i])
-#        #cslb_hierarchial_word.append(cslb_list[i])
-#    cslb_hierarchial_group.append(group)
-##print(cslb_hierarchial_group)
-#
-#vinson_hierarchial_word = []
-#vinson_hierarchial_group = []
-#for k in clusters_vinson:
-#    group = []
-#    for i in k:
-#        group.append(cslb_list[i])
-#        #vinson_hierarchial_word.append(cslb_list[i])
-#    vinson_hierarchial_group.append(group)
-##print(vinson_hierarchial_group)
-#
-#cslb_hierarchial_i = []
-#a = 0
-#while a < 8:
-##    if 11 < a and a < 20:
-##        a += 1
-##    else:
-#    for name in cslb_hierarchial_group[a]:
-#        cslb_hierarchial_word.append(name)
-#        i=0
-#        for row in cslb_vocab.loc[:,"cslb"]:
-#            if (name == cslb_vocab.loc[i,"cslb"]):
-#                cslb_hierarchial_i.append(i)
-#                break
-#            else:
-#                i += 1
-#    a += 1            
-##print(cslb_hierarchial_i) 
-#cslb_hierarchial = cslb.iloc[cslb_hierarchial_i]
-##print(cslb_hierarchial)
-#
-#vinson_hierarchial_i = []
-#b = 0
-#while b < 3:
-#    for name in vinson_hierarchial_group[b]:
-#        vinson_hierarchial_word.append(name)
-#        i=0
-#        for row in vinson_vocab.loc[:,"cslb"]:
-#            if (name == vinson_vocab.loc[i,"cslb"]):
-#                vinson_hierarchial_i.append(i)
-#                break
-#            else:
-#                i += 1
-#    b += 1
-##print(vinson_hierarchial_i) 
-#vinson_hierarchial = vinson.iloc[vinson_hierarchial_i]
-##print(vinson_hierarchial)
-
-#compare_norms(cslb_hierarchial,vinson_hierarchial,'cslb','vinson','eläimet')
-#
-#hierarchical_clustering(cslb_hierarchial,cslb_hierarchial_word,'14.')
-#hierarchical_clustering(vinson_hierarchial,vinson_hierarchial_word,'14.')
-
