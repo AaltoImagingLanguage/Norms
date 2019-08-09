eb524d8471432115d8b9fa688efde2fd03e416c5
Tiina Lindh-Knuutila <tiina.lindh-knuutila@aalto.fi>
2019-08-09 15:05:01 +0300

    Small changes to visualization, documentation and result saving

--- SOMvisualizations.m
+++ SOMvisualizations.m
@@ -9,7 +9,7 @@ msize = 75;
 shape = 'toroid';
 
 dpath = '/m/nbe/project/aaltonorms/';
-normset = {'aaltoprod'}
+normset = {'aaltoprod'};
 
 
 for n = 1:length(normset)
@@ -17,7 +17,7 @@ for n = 1:length(normset)
     vocab  = importdata([dpath 'data/' normset{n} '/vocab.csv']);
     [catind, catnames] = getCatInd(vocab, normset{n});
     data = textscan(fopen([dpath 'data/' normset{n} '/correspondence.csv'], 'r'), ...
-    '%n%s%s%*[^\n]', 'delimiter', '\t', 'headerlines', 1)
+    '%n%s%s%*[^\n]', 'delimiter', '\t', 'headerlines', 1);
     envocab = data{3}; 
 end
 
@@ -99,7 +99,7 @@ catgids = {abstract, concrete}
 
 %%Train map only on Abstract words
 
-catg = 1 % abstract
+catg = 1; % abstract
 Xn = vectormat(catgids{catg},:);
 
 sData = som_data_struct(Xn, 'labels', envocab(catgids{catg}));
@@ -123,17 +123,24 @@ abstractlabels={'highly abstract', 'medium abstract'};
 for i = 1:2
 hits2(i,:) = som_hits(sMap2,Xn(abstractness{i},:), 'crisp');
 end
+%%% Choosing the best K-means clustering on SOM 
+%Makes a k-means to the given data set with different values of
+%  k. The k-means is run multiple times for each k, and the best of
+%  these is selected based on sum of squared errors. Finally, the
+%  Davies-Bouldin index is calculated for each clustering. 
 
-[c,p,err,ind] = kmeans_clusters(sMap, 10)
-[dummy, id] = min(ind)
-mycolors=colormap(bone(id)); 
+[c,p,err,ind] = kmeans_clusters(sMap, 10);
+[dummy, id] = min(ind); %choose the one with the minimum
+                        %Davies-Bouldin index
+mycolors=colormap(bone(id+1)); 
 abstract_colormap=[mycolormap(12,:); mycolormap(15,:)]; 
 som_show(sMap,'color',{p{id},sprintf('%d clusters',id)}, 'footnote', ...
-         '', 'colormap', mycolors)
+         '', 'colormap', mycolors(1:end-1,:))
 
 som_show_add('hit', hits2', 'marker', 'pie', 'MarkerColor', ...
              abstract_colormap)
-som_show_add('label', sMap2.labels, 'textcolor', 'black')
+som_show_add('label', sMap2.labels, 'textcolor', 'black', 'textsize', ...
+             12)
 
 
 for ii = 1:length(abstractlabels)
@@ -149,12 +156,24 @@ som_show_add('hit', hits2', 'marker', 'pie', 'MarkerColor', [0,0,0;0.7, ...
 som_show_add('label', sMap2.labels, 'textcolor', 'blue')
 
 
+%store values in a struct
+abstract.sData = sData; 
+abstract.sMap = sMap;
+abstract.sMap2 = sMap2; %autolabeled version
+abstract.hits = hits2; 
+%store k-means clustering values
+abstract.c = c; 
+abstract.p = p; 
+abstract.err = err; 
+abstract.ind =ind; 
+
+
 %% Just concrete 
 
-aaltoprod_catids=find(not(cellfun(@isempty, catind)))
+aaltoprod_catids=find(not(cellfun(@isempty, catind)));
 aaltoprod_concreteids=aaltoprod_catids(find(cellfun(@isempty, ...
-                                                strfind(catnames(aaltoprod_catids), 'abstr'))))
-aaltoprod_concretelabels = catnames(aaltoprod_concreteids)
+                                                strfind(catnames(aaltoprod_catids), 'abstr'))));
+aaltoprod_concretelabels = catnames(aaltoprod_concreteids);
 
 catg = 2 % concrete
 Xn = vectormat(catgids{catg},:);
@@ -166,7 +185,6 @@ sMap = som_make(sData,'munits',60, 'shape', shape);
 [qe(n),te(n)] = som_quality(sMap,sData);
 
 sMap2 = som_autolabel(sMap, sData); 
-colormap(spring)
 figure; 
 som_show(sMap, 'empty', '', 'footnote', strcat(normset{n}, '-concrete')); 
 som_show_add('label', sMap2.labels)
@@ -194,11 +212,17 @@ end
 for i = 1:length(concretecatg)
 hits(i,:) = som_hits(sMap2,Xn(concretecatg{i},:), 'crisp');
 end
-colormap(bone)
-[c,p,err,ind] = kmeans_clusters(sMap, 10)
-[dummy, id] = min(ind)
-mycolors = colormap(bone(id)); 
-som_show(sMap,'color',{p{id},sprintf('%d clusters',id)}, 'footnote','', 'colormap',mycolors)
+%%% Choosing the best K-means clustering on SOM 
+%Makes a k-means to the given data set with different values of
+%  k. The k-means is run multiple times for each k, and the best of
+%  these is selected based on sum of squared errors. Finally, the
+%  Davies-Bouldin index is calculated for each clustering. 
+
+
+[c,p,err,ind] = kmeans_clusters(sMap, 10);
+[dummy, id] = min(ind); %choose the one with minimum Davies-Bouldin index
+mycolors = colormap(bone(id+1)); 
+som_show(sMap,'color',{p{id},sprintf('%d clusters',id)}, 'footnote','', 'colormap',mycolors(1:end-1,:))
 som_show_add('hit', hits', 'marker', 'pie', 'markercolor', mycolormap)
 %som_show_add('label', sMap2.labels, 'textcolor', 'blue')
 
@@ -211,7 +235,18 @@ legend(mypatch, aaltoprod_concretelabels, 'location', 'northeastoutside');
 %cb = colorbar;
 %set(cb, 'ticks', 1:id, 'ticklabels', []));
 %set(gca, 'clim', [0.5 5.5]);
+%store values in a struct
+concrete.sData = sData; 
+concrete.sMap = sMap;
+concrete.sMap2 = sMap2; %autolabeled version
+concrete.hits = hits; 
+%store k-means clustering values
+concrete.c = c; 
+concrete.p = p; 
+concrete.err = err; 
+concrete.ind =ind; 
 
-save(strcat(dpath,'figures/'datestr(now,'dd.mm.yyyy', 2), ...
+
+save(strcat(dpath,'figures/', datestr(now,'dd.mm.yyyy', 2), ...
             '__abstr_concrete_aaltonorms.mat')); 
 
