2f0dc4aba46295e3238143497b18abf213bcff06
Kivisaari Sasa <sasa.kivisaari@aalto.fi>
2019-08-17 14:19:09 +0300

    Remove duplicate files

--- matfunc/getCatMembers.m
+++
@@ -1,8 +0,0 @@
-function [tokens] = getCatMembers(catname)
-
-%returns all members in a particular category
-
-fn = '/m/nbe/project/aaltonorms/data/SuperNormList.xlsx'; 
-opts = detectImportOptions(fn);       
-T = readtable(fn,opts);
-tokens = T(ismember(T.category, catname),:);
